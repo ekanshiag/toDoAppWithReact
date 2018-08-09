22e23653ff9ba090593d5c75f4fc74a15287499e
Ekanshi Agrawal <ekanshi@gmail.com>
2018-08-09 11:28:26 +0530

    Make Options a functional component

--- src/Options.js
+++ src/Options.js
@@ -1,40 +1,36 @@
 import React, { Component } from 'react'
 import './Options.css'
 
-class Options extends Component {
-  render () {
-    return (
-      <div className='optionsView'>
-        <label id='noteLabel' htmlFor='taskNote'>Notes</label>
-        <textarea
-          id='taskNote'
-          disabled={!this.props.editable}
-          onChange={(event) => this.props.onUpdateNotes(event.target.value)}
-          value={this.props.notes}
-        />
-        <label id='dueLabel' htmlFor='taskDueDate'>Due Date</label>
-        <input
-          type='Date'
-          id='taskDueDate'
-          value={this.props.dueDate}
-          disabled={!this.props.editable}
-          onChange={(event) => this.props.onUpdateDue(event.target.value)}
-        />
-        <label id='priorLabel' htmlFor='taskPriority'>Priority</label>
-        <select
-          id='taskPriority'
-          value={this.props.priority}
-          disabled={!this.props.editable}
-          onChange={(event) => this.props.onUpdatePrior(event.target.value)}
-        >
-          <option value='' disabled>Please select one</option>
-          <option value='Low'>Low</option>
-          <option value='Medium'>Medium</option>
-          <option value='High'>High</option>
-        </select>
-      </div>
-    )
-  }
+export default function Options (props) {
+  return (
+    <div className='optionsView'>
+      <label id='noteLabel' htmlFor='taskNote'>Notes</label>
+      <textarea
+        id='taskNote'
+        disabled={!props.editable}
+        onChange={(event) => props.onUpdateNotes(event.target.value)}
+        value={props.notes}
+      />
+      <label id='dueLabel' htmlFor='taskDueDate'>Due Date</label>
+      <input
+        type='Date'
+        id='taskDueDate'
+        value={props.dueDate}
+        disabled={!props.editable}
+        onChange={(event) => props.onUpdateDue(event.target.value)}
+      />
+      <label id='priorLabel' htmlFor='taskPriority'>Priority</label>
+      <select
+        id='taskPriority'
+        value={props.priority}
+        disabled={!props.editable}
+        onChange={(event) => props.onUpdatePrior(event.target.value)}
+      >
+        <option value='' disabled>Please select one</option>
+        <option value='Low'>Low</option>
+        <option value='Medium'>Medium</option>
+        <option value='High'>High</option>
+      </select>
+    </div>
+  )
 }
-
-export default Options
